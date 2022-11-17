8c51ac45866931ed7c4511f8dd26877bc0a195e8
dream <dream_chou93@163.com>
2022-11-17 11:29:30 +0800

    增加用户存储

--- util/SqlUtil.ts
+++ util/SqlUtil.ts
@@ -15,15 +15,19 @@ export module SqlUtil {
 
     let _key: string | null = null;
     let _iv: string | null = null;
+    let _userId: string = '';
+    let _userKeys: string[] | null = [];
 
     /**
      * 初始化密钥
      * @param key aes加密的key 
-     * @param iv aes加密的iv
+     * @param iv aes加密的iv]
+     * @param userId 用户id
      */
-    export function init(key: string, iv: string) {
+    export function init(key: string, iv: string, userId: string | null = null) {
         _key = md5(key);
         _iv = md5(iv);
+        _userId = userId || '';
     }
 
     /**
@@ -138,5 +142,53 @@ export module SqlUtil {
      */
     export function clear() {
         sys.localStorage.clear();
+    }
+
+    /**
+     * 存储用户数据
+     * @param key 
+     * @param value 
+     */
+    export function setUserData(key: string, value: any) {
+        addUserKey(key);
+        set(_userId + key, value);
+    }
+
+    /**
+     * 获取用户数据
+     * @param key 
+     * @param defaultValue 
+     * @returns 
+     */
+    export function getUserData(key: string, defaultValue: any): any {
+        return get(_userId + key, defaultValue);
+    }
+
+    /**
+     * 移除用户指定的数据
+     * @param key 
+     */
+    export function removeUserData(key: string) {
+        remove(_userId + key);
+    }
+
+    /**
+     * 移除用户所有本地数据
+     */
+    export function cleanUserData() {
+        let userKeys: string[] = get('user_save_keys', []);
+        for (let i = 0; i < userKeys.length; i++) {
+            remove(_userId + userKeys[i]);
+        }
+    }
+
+    function addUserKey(key: string) {
+        if (!_userKeys) {
+            _userKeys = get('user_storage_key', []);
+        }
+        if (_userKeys!.indexOf(key) === -1) {
+            _userKeys!.push(key);
+            set('user_storage_key', _userKeys);
+        }
     }
 }
